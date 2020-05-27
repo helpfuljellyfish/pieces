6664c29d00702072e052ea5b77306f776d479cbd
helpfuljellyfish <65994694+helpfuljellyfish@users.noreply.github.com>
2020-05-27 00:05:21 -0400

    Update confirm-delivery.js

--- confirm-delivery.js
+++ confirm-delivery.js
@@ -1,6 +1,6 @@
 const Airtable = require('airtable');
 const base = new Airtable({ apiKey: 'keyEJTQQlKRGQCuab' })
-  .base('your base');
+  .base('appJlK7seVbzYDCnO');
 
 exports.handler = (context, {subscriber, delivery, confirmed, response}, done) =>
   base('confirmations').create([
